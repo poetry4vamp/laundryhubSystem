0c0b652926905541ce3ccf99ea746e95a2ea0d6e
Allana Grace Ubaldo <75129917+aalyaanna@users.noreply.github.com>
2023-07-08 11:10:53 +0800

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const dashboardController = require('../controller/dashboardController');
 
 router.get('/dashboard', dashboardController.getDashboard);
-router.post('/dashboard', dashboardController.postDashboard);
+
 
 module.exports = router;
