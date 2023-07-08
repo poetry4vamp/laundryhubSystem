7ab542b56c23f1788c1fd4c9b8425734c5a09002
poetry4vamp <133098907+poetry4vamp@users.noreply.github.com>
2023-07-08 20:50:22 +0800

    Delete reservation.js

--- routes/reservation.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reservationController = require('../controller/reservationController');
-
-router.get('/reservation', reservationController.getReservation);
-router.post('/reservation', reservationController.postReservation);
-
-module.exports = router;
